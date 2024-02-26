342242db577530d177b3dddbcd90e5443d556e37
Bikas0 <bikasictiu1718@gmail.com>
2024-02-26 10:52:09 +0600

    create packages

---
+++ test_package.py
@@ -0,0 +1,6 @@
+from PracticePackage.PackageA import f1, f2
+from PracticePackage.PackageA.subPackageA import f3, f4
+from PracticePackage.PackageA.subPackageB import f5
+
+print(f"f1 package: {f1.print_something}")
+print(f2.print_something)
