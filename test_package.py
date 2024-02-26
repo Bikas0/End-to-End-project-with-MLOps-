9be9477cc96da5cd5dac8c0a95a65ef59c325f10
Bikas0 <bikasictiu1718@gmail.com>
2024-02-26 16:07:44 +0600

    update code

--- test_package.py
+++ test_package.py
@@ -1,6 +1,9 @@
 from PracticePackage.PackageA import f1, f2
 from PracticePackage.PackageA.subPackageA import f3, f4
 from PracticePackage.PackageA.subPackageB import f5
+import PracticePackage
+import pathlib
 
+
+print(pathlib.Path(PracticePackage.__file__).resolve().parent)
 print(f"f1 package: {f1.print_something}")
-print(f2.print_something)
